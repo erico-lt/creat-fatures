412b00bf64842b0e4924fdadf157c8eba19ae002
Érico Santana <75767727+erico-lt@users.noreply.github.com>
2023-04-04 07:02:59 -0300

    Mudando o construtor
    .

--- src/model/entites/intens/Ball.java
+++ src/model/entites/intens/Ball.java
@@ -4,8 +4,8 @@ import model.entites.Item;
 
 public class Ball extends Item{
 
-    public Ball(String model, Double price, Integer quant) {
-        super(model, price, quant);       
+    public Ball(String nome,String model, Double price, Integer quant) {
+        super(nome, model, price, quant);       
     }
 
     @Override
